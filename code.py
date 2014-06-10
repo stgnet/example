6781627a375c82e303ff801be1573784b1097704
Scott Griepentrog <scott@griepentrog.com>
2014-06-10 12:22:23 -0500

    first test example

---
+++ code.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+import random
+
+def my_func():
+    return random.randint(0,10)
+
+for code in range(0, 10):
+        print my_func()
---
+++ test.py
@@ -0,0 +1,6 @@
+import code
+
+def test_range():
+    x = code.my_func()
+    assert x > 0
+    assert x < 10
